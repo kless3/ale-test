9bf3f51028140b72dda8be16bf4584e258817631
kless <maksefimchick@gmail.com>
2025-07-11 15:46:24 +0300

    add piska

--- src/main/java/anton/HelloController.java
+++ src/main/java/anton/HelloController.java
@@ -1,13 +1,18 @@
 package anton;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello-world")
+
+    private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @GetMapping("/hey")
     public String sayHello() {
-        return "hello_word";
+        logger.info("Hey endpoint called");
+        return "hey";
     }
 }
